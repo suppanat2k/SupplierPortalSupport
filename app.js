2a1861beff435264a5699c122c73baa9ad1c557a
suppanat2k <suppanat2k@gmail.com>
2022-06-26 00:30:34 +0700

    about update non-res 1.0.4

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-document.getElementById("button1").addEventListener("click", function(){
-    document.querySelector(".video1").style.display = "flex";
-})
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
+
+
+// document.getElementById("button1").addEventListener("click", function(){
+//     document.querySelector(".video1").style.display = "flex";
+// })
